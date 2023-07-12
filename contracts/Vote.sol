e014b6917ecdfd1693abbdbb7f5367508a7f9c46
ironp <devascovitche@gmail.com>
2023-07-12 12:09:54 +0300

    set favorite for removal scores in future

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -6,6 +6,8 @@ contract Vote {
     address token;
     uint8 public interest = 10;
     uint public expiration = 30;
+    uint public fullExpiration = 60 * 60 * 24 * 365;
+    uint public partExpiration = 60 * 60 * 24 * 30;
 //    uint public expiration = 60 * 60 * 24 * 30;
 
     mapping(address => address) owners;
@@ -14,26 +16,27 @@ contract Vote {
     mapping(address => mapping(uint256 => uint256)) public deposits; // address => [block.timestamp => msg.value]
     mapping(address => uint256) public depositsCount; // address => id(counted++);
     mapping(address => mapping(uint256 => uint256)) public depositIDTimes; // address => [id(counted++) => block.timestamp]
+    mapping(address => mapping(uint256 => string)) public depositFavorite; // address => [id(counted++) => hero]
 
     modifier onlyOwners() {
         require(owners[msg.sender] != address(0), 'Only owners can set new admin.');
         _;
     }
 
-    constructor(address _token, string memory _hero1, string memory _hero2) public {
+    constructor(address _token, string memory _hero1, string memory _hero2) {
         owners[msg.sender] = msg.sender;
 
         token = _token;
-        heroes[_hero1] = 0;
-        heroes[_hero2] = 0;
+        heroes[_hero1] = 100;
+        heroes[_hero2] = 100;
     }
 
     function setOwner(address _owner) public onlyOwners {
         owners[_owner] = _owner;
     }
 
-    function deposit(string _heroName) public payable {
-        require(heroes[_heroName] != 0, 'Hero not found.');
+    function deposit(string memory _heroName) public payable {
+        require(heroes[_heroName] >= 100, 'Hero not found.');
 
         uint count = depositsCount[msg.sender] + 1;
         uint time = block.timestamp;
@@ -41,11 +44,12 @@ contract Vote {
         deposits[msg.sender][time] = msg.value;
         depositsCount[msg.sender] = count;
         depositIDTimes[msg.sender][count] = time;
+        depositFavorite[msg.sender][count] = _heroName;
 
         heroes[_heroName] = heroes[_heroName] + msg.value;
     }
 
-    function withdraw(address _holder, uint256 _depositStart) public payable {
+    function withdraw(address payable _holder, uint256 _depositStart) public payable {
         require(deposits[_holder][_depositStart] != 0, 'Deposit not found.');
         require(block.timestamp - _depositStart >= expiration, 'Deposit is not expired.');
         require(msg.sender == _holder, 'Only owner can withdraw.');
@@ -54,7 +58,7 @@ contract Vote {
         uint256 interestAmount = amount * interest / 100;
         uint256 totalAmount = amount + interestAmount;
 
-        msg.sender.transfer(totalAmount);
+        _holder.transfer(totalAmount);
 
         deposits[_holder][_depositStart] = 0;
     }
